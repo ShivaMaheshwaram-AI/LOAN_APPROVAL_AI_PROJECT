2a7a085b311c1142e81adf25f57bc7b394405102
ShivaMaheshwaram-AI <shivamaheshwaramceo@gmail.com>
2024-09-02 14:07:01 +0530

    v8 created distribution file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = "shiva_loan_approval_ai_project",
     version = "0.0.1",
-    author = "shiva maheshwaram"
+    author = "shiva maheshwaram",
     author_email = "shivamaheshwaram1995@gmail.com",
     url = "https://github.com/ShivaMaheshwaram-AI",
     description = "a simple loan approval prediction project using linear regression",
